8f9f10d40373ebf77da8d164c3f997f9c4905e98
yangyi02 <yangyi02@gmail.com>
2017-10-01 13:52:47 -0700

    add motion range 4, no difference

---
+++ exp007/exp06/train.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ../../set_path.sh
+
+CUDA_VISIBLE_DEVICES=1 python ../demo.py --train --data=nyuv2 --batch_size=64 --image_size=64 --motion_range=4 --num_frame=4 --train_epoch=2000 --test_interval=200 --test_epoch=20 --learning_rate=0.001 2>&1 | tee train.log
